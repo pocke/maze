5ceb38b8e157dba2f69b1a9cd57d8fc225c158c3
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-07-20 21:18:19 +0900

    Drop category tree

--- app/models/category.rb
+++ app/models/category.rb
@@ -13,9 +13,4 @@ class Category < ApplicationRecord
   has_one :post_source
   has_many :subscribes
   has_many :users, through: :subscribes
-
-  has_many :child_category_trees,  class_name: :CategoryTree, foreign_key: :child_id
-  has_many :parent_category_trees, class_name: :CategoryTree, foreign_key: :parent_id
-  has_many :children, through: :parent_category_trees
-  has_many :parents,  through: :child_category_trees
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,15 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  describe 'with children relation' do
-    let(:parent){create :category}
-    let(:child){create :category}
-    let(:relation){create :category_tree, parent: parent, child: child}
-
-    it 'should set parent-child relation' do
-      relation
-      is_asserted_by{ parent.children == [child] }
-      is_asserted_by{ child.parents == [parent] }
-    end
-  end
 end
